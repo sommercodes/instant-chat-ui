496ff5cebb9d8fd9eae6fa8fde1f88287577d094
Sommer <sommershurbaji23@gmail.com>
2017-08-06 21:53:35 -0400

    removing amazon deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiURL: 'https://instant-chat-server.herokuapp.com',
-  apiURL: 'http://instant-chat-server.b4u8rxpwdu.ca-central-1.elasticbeanstalk.com'
+  apiURL: 'https://instant-chat-server.herokuapp.com',
+  // apiURL: 'http://instant-chat-server.b4u8rxpwdu.ca-central-1.elasticbeanstalk.com'
 };
